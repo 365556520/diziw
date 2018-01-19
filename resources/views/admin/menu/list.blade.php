f3728fb404277f42d779bb9d4423dda6f96dbab7
365556520 <33852383+365556520@users.noreply.github.com>
2018-01-19 11:37:07 +0800

    2

--- resources/views/admin/menu/list.blade.php
+++ resources/views/admin/menu/list.blade.php
@@ -16,7 +16,7 @@
     <div class="row ">
         <div class="col-md-12 col-sm-12 col-xs-12">
             <!-- left panel -->
-            <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="col-md-6 col-sm-12 col-xs-12">
                 <div class="x_panel">
                     <div class="x_title">
                         <h2>Pop Overs <small>Sessions</small></h2>
@@ -80,7 +80,7 @@
             </div>
             <!-- end left panel -->
             <!-- right panel -->
-            <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="col-md-6 col-sm-12 col-xs-12">
                 <div class="x_panel">
                     <div class="x_title">
                         <h2>Form Basic Elements <small>different form elements</small></h2>
