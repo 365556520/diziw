c9933593a93c0b9f4ada38e9a7a6c6f1bac2cc69
365556520 <33852383+365556520@users.noreply.github.com>
2018-01-26 10:23:27 +0800

    2

--- app/Notifications/RestPassword.php
+++ app/Notifications/RestPassword.php
@@ -43,7 +43,7 @@ class RestPassword extends Notification
             ->subject('XXXX')
             ->salutation('XXX')
             ->line('您之所以收到这封邮件是因为我们收到了您重置密码的申请。')
-            ->action('Reset Password', url(config('app.url').route('password.reset', $this->token, false)))
+            ->action('点击修改密码', url(config('app.url').route('password.reset', $this->token, false)))
             ->line('如果您本人未进行密码重置，您可以不必采取进一步操作!');
     }
     /**
