43ef530ba6110b73e78717ef782bea838be6e71d
365556520 <33852383+365556520@users.noreply.github.com>
2018-02-10 20:18:24 +0800

    2

--- resources/views/admin/permission/show.blade.php
+++ resources/views/admin/permission/show.blade.php
@@ -3,29 +3,25 @@
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
         &times;
     </button>
-    <h4 class="modal-title" id="myModalLabel">{{trans('admin/permission.show')}}</h4>
+    <h4 class="modal-title " id="myModalLabel">{{$permission->display_name}}<br>
+        <small>{{trans('admin/permission.model.created_at')}}:{{$permission->created_at}}</small>
+    </h4>
 </div>
-<div class="modal-body">
-    <div class="row">
-        <label class="control-label col-md-6 col-sm-6 col-xs-12" for="first-name">{{trans('admin/permission.model.display_name')}}:{{$permission->display_name}}<br>
-            <small>{{trans('admin/permission.model.created_at')}}：{{$permission->created_at}}</small>
-        </label>
-    </div>
-    <div class="row">
-        <label class="control-label col-md-6 col-sm-6 col-xs-12" for="first-name">{{trans('admin/permission.model.name')}}:{{$permission->name}}<br>
-        </label>
-    </div>
-    <div class="row">
-        <label class="control-label col-md-6 col-sm-6 col-xs-12" for="first-name">{{trans('admin/permission.model.description')}}:{{$permission->description}}<br>
-        </label>
-    </div>
-
-<div class="modal-footer">
-    <div class="form-group">
-        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-            <button type="button" class="btn btn-default" data-dismiss="modal" >关闭
-            </button>
+<div class="modal-body ">
+    <div class="row ">
+        <div class="control-label col-md-6 col-sm-6 col-xs-12 " >{{trans('admin/permission.model.name')}}:{{$permission->name}}<br>
         </div>
     </div>
+    <div class="row">
+        <div class="control-label col-md-6 col-sm-6 col-xs-12 " >
+            {{trans('admin/permission.model.description')}}:
+            <span class="text-danger">{{$permission->description}}</span>
+        </div>
+    </div>
+</div>
+<div class="modal-footer">
+        <small class="text-success">
+            {{trans('admin/permission.model.updated_at')}}:{{$permission->updated_at}}
+        </small>
 </div>
 
