718c89378bd7af0eeaba35d71d2a75e4bcc01fc6
365556520 <33852383+365556520@users.noreply.github.com>
2018-02-22 11:14:07 +0800

    写到角色添加的角色绑定，目前只获取到所有权限

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,17 +26,17 @@
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
                         <h1>{{trans('auth/login.title.admin')}}</h1>
-                            <div class="row">
-                                <div class="row form-group">
-                                    {{--帐号密码和ciook--}}
-                                    <div class="col-md-12 col-sm-12 col-xs-12  has-feedback {{ $errors->has(config('admin.globals.username')) ? ' has-error' : '' }}">
-                                        <input type="text" id="config('admin.globals.username')" class="form-control has-feedback-left "  placeholder="{{trans('auth/login.loginform.username')}}" required name="{{config('admin.globals.username')}}" value="{{ old(config('admin.globals.username')) }}">
-                                        <span class="fa fa-user form-control-feedback left" ></span>
 
-                                    </div>
+                            <div class="form-group">
+                                {{--帐号密码和ciook--}}
+                                <div class="col-md-12 col-sm-12 col-xs-12  has-feedback {{ $errors->has(config('admin.globals.username')) ? ' has-error' : '' }}">
+                                    <input type="text" id="config('admin.globals.username')" class="form-control has-feedback-left "  placeholder="{{trans('auth/login.loginform.username')}}" required name="{{config('admin.globals.username')}}" value="{{ old(config('admin.globals.username')) }}">
+                                    <span class="fa fa-user form-control-feedback left" ></span>
+
                                 </div>
                             </div>
-                            <div class="row">
+
+                            <div class="form-group">
                                 <div class="col-md-12 col-sm-12 col-xs-12  has-feedback {{ $errors->has('password') ? ' has-error' : '' }}">
                                     <input type="password" id="password" class="form-control has-feedback-left "  placeholder="{{trans('auth/login.loginform.password')}}"  name="password" >
                                     <span class="fa fa-lock form-control-feedback left" ></span>
@@ -44,31 +44,28 @@
                                 </div>
                             </div>
 
-                            <div class="row ">
-                                <div class="col-md-7 col-sm-7 col-xs-7 {{ $errors->has('captcha') ? ' has-error' : '' }} ">
-                                    <input type="text" id="captcha" class="form-control "  placeholder="{{trans('auth/login.captcha')}}"  name="captcha" >
+                        <div class="form-group ">
+                            <div class="col-md-7 col-sm-7 col-xs-7 {{ $errors->has('captcha') ? ' has-error' : '' }} ">
+                                <input type="text" id="captcha" class="form-control "  placeholder="{{trans('auth/login.captcha')}}"  name="captcha" >
 
-                                </div>
-                                <div class="col-md-5 col-sm-5 col-xs-5">
-                                    <img src="{{captcha_src()}}" style="cursor: pointer;" onclick="this.src='{{captcha_src()}}'+Math.random()">
-                                </div>
                             </div>
-                            <div class="row">
-                                <div class="col-md-12 col-sm-12 col-xs-12  checkbox">
-                                    <label class="left">
-                                        <input type="checkbox"  name="remember" {{ old('remember') ? 'checked' : '' }}> {{trans('auth/login.loginform.rememberPassword')}}
-                                    </label>
-                                </div>
+                            <div class="col-md-5 col-sm-5 col-xs-5">
+                                <img src="{{captcha_src()}}" style="cursor: pointer;" onclick="this.src='{{captcha_src()}}'+Math.random()">
                             </div>
-                        <br>
-                            <div class="row">
-                                <div class="col-md-12 col-sm-12 col-xs-12 ">
-                                    <button class="btn btn-dar btn-sm submit" type="submit" >{{trans('auth/login.loginform.submit')}}</button>
-                                    <a class="reset_pass" data-toggle="modal" data-target="#resetModal" href="{{ route('password.request') }}">
-                                        {{trans('auth/login.loginform.Lost_your_password')}}
-                                    </a>
-                                </div>
+                            <div class="col-md-12 col-sm-12 col-xs-12  checkbox">
+                                <label class="pull-left">
+                                    <input type="checkbox"  name="remember" {{ old('remember') ? 'checked' : '' }}> {{trans('auth/login.loginform.rememberPassword')}}
+                                </label>
                             </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-12 col-sm-12 col-xs-12 ">
+                                <button class="btn btn-dar btn-sm submit" type="submit" >{{trans('auth/login.loginform.submit')}}</button>
+                                <a class="reset_pass" data-toggle="modal" data-target="#resetModal" href="{{ route('password.request') }}">
+                                    {{trans('auth/login.loginform.Lost_your_password')}}
+                                </a>
+                            </div>
+                        </div>
                         <div class="clearfix"></div>
                         <div class="separator">
                             <p class="change_link">
