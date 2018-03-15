35f98ea2ce7ea25423c5d32b743af373685eceda
365556520 <33852383+365556520@users.noreply.github.com>
2018-03-15 09:56:21 +0800

    xiao

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,13 +4,12 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
-{
+class CreateUsersTable extends Migration{
     /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+ * Run the migrations.
+ *
+ * @return void
+ */
     public function up(){
         //用户表
         Schema::create('users', function (Blueprint $table) {
@@ -22,8 +21,6 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-
-
         //用户信息表
         Schema::create('user_data', function (Blueprint $table) {
             $table->increments('id');
@@ -34,18 +31,13 @@ class CreateUsersTable extends Migration
             $table->string('ipone')->nullable();
             $table->string('qq')->nullable();
             $table->string('headimg')->nullable();
-            /*家庭住址*/
-            $table->string('address')->nullable();
-//            个人爱好
-            $table->string('hobby')->nullable();
-//            自述
-            $table->string('Readme')->nullable();
+            $table->string('address')->nullable(); /*家庭住址*/
+            $table->string('hobby')->nullable();//   个人爱好
+            $table->string('Readme')->nullable();//     自述
             //外检约束更新和删除都绑定
-            $table->foreign('user_id')->references('id')->on('users')
-                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users') ->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
-
     }
 
     /**
