6dc5a78e2744802f51da2fdd9cdeac7886064128
365556520 <33852383+365556520@users.noreply.github.com>
2018-05-25 17:41:05 +0800

    最新测试

--- webapp/src/components/HelloWorld.vue
+++ webapp/src/components/HelloWorld.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="hello">
-    <mt-button @click.native="handleClick">按钮</mt-button>
+    <mt-header title="标题过长会隐藏后面的内容啊哈哈哈哈">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
   </div>
 </template>
 
@@ -12,11 +17,6 @@
         msg: 'Welcome to Your Vue.js App'
       }
     },
-      methods: {
-          handleClick: function() {
-              this.$toast('Hello world!');
-          }
-      }
   }
 </script>
 
