1f75487be45def9babe07a3e21946ae2d216a4c5
365556520 <33852383+365556520@users.noreply.github.com>
2018-06-23 23:44:49 +0800

    第一版本

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,14 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-/*跨域接口响应*/
-header('Access-Control-Allow-Origin:*');
 
+/*跨域接口响应*/
+// 指定允许其他域名访问
+header("Access-Control-Allow-Origin:*");
+// 响应类型
+header("Access-Control-Allow-Methods:POST,GET");
+// 响应头设置
+header("Access-Control-Allow-Headers:x-requested-with,content-type");
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
