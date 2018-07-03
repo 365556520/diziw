f0da5a6c51f95d3652de4a10881f84be7cce8fcd
365556520 <33852383+365556520@users.noreply.github.com>
2018-07-03 12:01:10 +0800

    最新测试

--- routes/api/content.php
+++ routes/api/content.php
@@ -10,7 +10,7 @@ Route::group(['namespace'=>'Api'],function(){
     //视频标签接口
     Route::get('videoTags','ContentController@videoTags');
     //视频类接口
-    Route::get('videoclasss/{id}','ContentControllpsotr@videoclasss');
+    Route::get('videoclasss/{id}','ContentController@videoclasss');
     //推荐视频接口
     Route::get('commendvideoclass/{row}','ContentController@commendvideoclass');
     //热门视频接口
