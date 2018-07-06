28483c563a42bf4030c03e4b738bf07f41f4bd6f
365556520 <33852383+365556520@users.noreply.github.com>
2018-07-06 22:40:16 +0800

    第一版本

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,11 @@ use Illuminate\Http\Request;
 header('Access-Control-Allow-Origin: *');
 // 响应类型
 header('Access-Control-Allow-Methods: GET, POST, PUT,DELETE');
-// 响应头设置
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+// 响应头设置 lavarel响应头要和vue中设置的响应头对应都不可缺（要不存在响应头错误）
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept,Authorization");
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+//导入路由文件
 include __DIR__.'/api/content.php';
