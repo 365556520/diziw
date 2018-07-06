3747dee42e812c3042999524ce6641a37b833cde
365556520 <33852383+365556520@users.noreply.github.com>
2018-07-06 18:48:25 +0800

    最新测试

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,12 @@ use Illuminate\Http\Request;
 
 /*跨域接口响应*/
 // 指定允许其他域名访问
-header("Access-Control-Allow-Origin:*");
+header('Access-Control-Allow-Origin: *');
 // 响应类型
-header("Access-Control-Allow-Methods:POST,GET");
+header('Access-Control-Allow-Methods: GET, POST, PUT,DELETE');
 // 响应头设置
-header("Access-Control-Allow-Headers:x-requested-with,content-type");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
