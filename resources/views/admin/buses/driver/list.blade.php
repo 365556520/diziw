4b3e44d274e58e15e5383913e136c1ec3bd896c4
365556520 <33852383+365556520@users.noreply.github.com>
2018-08-05 18:59:41 +0800

    第一版本

--- resources/views/admin/buses/driver/list.blade.php
+++ resources/views/admin/buses/driver/list.blade.php
@@ -59,24 +59,50 @@
                                 <div class="layui-tab-item">
                                     <form class="layui-form " method="post" action="{{url('admin/driver')}}">
                                         {{csrf_field()}}
-                                        <div class="layui-form-item">
-                                            <label class="layui-form-label">起点</label>
-                                            <div class="layui-input-block">
-                                                <input type="text" name="buses_start"  required="required" autocomplete="off" placeholder=""class="layui-input">
+                                        <div class="layui-row">
+                                            <div class="layui-col-xs12 layui-col-sm12 layui-col-md4">
+                                                <div class="layui-form-item">
+                                                    <label class="layui-form-label">姓名</label>
+                                                    <div class="layui-input-block">
+                                                        <input type="text" name="username" lay-verify="required" placeholder="请输入驾驶员姓名" autocomplete="off" class="layui-input">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="layui-col-xs12 layui-col-sm12 layui-col-md4">
+                                                <div class="layui-form-item">
+                                                    <label class="layui-form-label">年龄</label>
+                                                    <div class="layui-input-block">
+                                                        <input type="text" name="username" lay-verify="required" placeholder="请输入年龄" autocomplete="off" class="layui-input">
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
-                                        <div class="layui-form-item">
-                                            <label class="layui-form-label">途经</label>
-                                            <div class="layui-input-block">
-                                                <input type="text" name="buses_midway" value="" data-role="tagsinput" >
+                                        <div class="layui-row">
+                                            <div class="layui-col-xs12 layui-col-sm12 layui-col-md4">
+                                                <div class="layui-form-item">
+                                                    <label class="layui-form-label">单选框</label>
+                                                    <div class="layui-input-block">
+                                                        <input type="radio" name="sex" value="男" title="男" checked="">
+                                                        <input type="radio" name="sex" value="女" title="女">
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
-                                        <div class="layui-form-item">
-                                            <label class="layui-form-label">终点</label>
-                                            <div class="layui-input-block">
-                                                <input type="text" name="buses_end"  required="required" autocomplete="off" placeholder=""class="layui-input">
+                                        <div class="layui-row" >
+                                            <div class="layui-col-xs12 layui-col-sm12 layui-col-md4">
+                                                <div class="layui-upload-drag" id="upload">
+                                                    <div id="uptitle">
+                                                        <i class="layui-icon"></i>
+                                                        <p>上传驾驶员头像</p>
+                                                        <p>点击或将图片拖拽到此处</p>
+                                                    </div>
+                                                    <img class="layui-upload-img  img-responsive col-md-4 col-sm-4 col-xs-8 " alt="" id="demo1"/>
+                                                    <input type="hidden" name="preview"  id="uploadimg" value="0">
+                                                </div>
                                             </div>
                                         </div>
+
+
                                         <div class="layui-form-item">
                                             <button class="layui-btn" lay-submit="" lay-filter="demo2">添加驾驶员</button>
                                         </div>
@@ -109,11 +135,20 @@
         $(function () {
             driverList.init();
         });
-        layui.use('element', function(){
-            var $ = layui.jquery
-                ,element = layui.element; //Tab的切换功能，切换事件监听等，需要依赖element模块
+        layui.use(['element','upload','form'],function(){
+            var $ = layui.jquery,
+            form = layui.form,
+            element = layui.element,//Tab的切换功能，切换事件监听等，需要依赖element模块
+            upload = layui.upload;
+            //拖拽上传
+            upload.render({
+                elem: '#upload'
+                ,url: '/upload/'
+                ,done: function(res){
+                    console.log(res)
+                }
+            });
         });
-
     </script>
     {{--提示代码--}}
     @include('component.errorsLayer')
