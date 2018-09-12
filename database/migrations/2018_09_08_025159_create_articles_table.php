27dacd06a4d928a7c9df8492b0cf79265ef1db2a
365556520 <33852383+365556520@users.noreply.github.com>
2018-09-12 10:58:34 +0800

    最新测试

--- database/migrations/2018_09_08_025159_create_articles_table.php
+++ database/migrations/2018_09_08_025159_create_articles_table.php
@@ -8,11 +8,23 @@ class CreateArticlesTable extends Migration
 {
     /**
      * Run the migrations.
-     *文章表
+     *文章
      * @return void
      */
     public function up()
     {
+        /*文章分类列表*/
+        Schema::create('categorys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('cate_name',100)->default("")->comment('分类名字');
+            $table->string('cate_keywords',100)->default("")->comment('关键词');
+            $table->string('cate_description',150)->default("")->comment('描述');
+            $table->string('cate_view')->default("")->comment('查看次数');
+            $table->integer('cate_order')->default("")->comment('分类排序');
+            $table->integer('cate_pid')->default("")->comment('父类id');
+            $table->timestamps();
+        });
+        /*文章表*/
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title',100)->default("")->comment('标题');
@@ -34,6 +46,7 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('category');
         Schema::dropIfExists('articles');
     }
 }
