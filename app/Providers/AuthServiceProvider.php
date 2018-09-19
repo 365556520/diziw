3acbe81667ca3869b3da11dc58985f89bfac4041
qiangge <365556520@qq.com>
2018-09-19 19:13:50 +0800

    恢复后的

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,6 +26,9 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+/*        //设置私人令牌的有效果时间 这里设置为1天
+        Passport::tokensExpireIn(Carbon::now()->addDays(1));
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(1));*/
         //该方法将会为颁发访问令牌、撤销访问令牌、客户端以及私人访问令牌注册必要的路由
         Passport::routes();
     }
