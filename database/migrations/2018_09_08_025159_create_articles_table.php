b390765b97fe04745e0afbde1230f09505a8a5d4
365556520 <33852383+365556520@users.noreply.github.com>
2018-09-20 11:53:17 +0800

    asd

--- database/migrations/2018_09_08_025159_create_articles_table.php
+++ database/migrations/2018_09_08_025159_create_articles_table.php
@@ -16,24 +16,24 @@ class CreateArticlesTable extends Migration
         /*文章分类列表*/
         Schema::create('categorys', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('cate_name',100)->default("")->comment('分类名字');
-            $table->string('cate_keywords',100)->default("")->comment('关键词');
-            $table->string('cate_description',150)->default("")->comment('描述');
+            $table->string('cate_name')->default("")->comment('分类名字');
+            $table->string('cate_keywords')->default("")->comment('关键词');
+            $table->string('cate_description')->default("")->comment('描述');
             $table->string('cate_view')->default("")->comment('查看次数');
-            $table->integer('cate_order')->default("")->comment('分类排序');
-            $table->integer('cate_pid')->default("")->comment('父类id');
+            $table->integer('cate_order')->default(0)->comment('分类排序');
+            $table->integer('cate_pid')->default(0)->comment('父类id');
             $table->timestamps();
         });
         /*文章表*/
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title',100)->default("")->comment('标题');
-            $table->string('tag',100)->default("")->comment('关键词');
-            $table->string('description',150)->default("")->comment('描述');
+            $table->string('title')->default("")->comment('标题');
+            $table->string('tag')->default("")->comment('关键词');
+            $table->string('description')->default("")->comment('描述');
             $table->string('thumb')->default("")->comment('缩略图');
             $table->string('view')->default("")->comment('查看次数');
-            $table->integer('category_id')->default("")->comment('分类id');
-            $table->integer('user_id')->default("")->comment('作者id');
+            $table->integer('category_id')->default(0)->comment('分类id');
+            $table->integer('user_id')->default(0)->comment('作者id');
             $table->text('content')->comment('内容');
             $table->timestamps();
         });
