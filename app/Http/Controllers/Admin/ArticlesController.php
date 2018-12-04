88631c11f8919b0f1304b30ae72cdb347e3e9459
365556520 <33852383+365556520@users.noreply.github.com>
2018-12-04 17:23:16 +0800

    asd

--- app/Http/Controllers/Admin/ArticlesController.php
+++ app/Http/Controllers/Admin/ArticlesController.php
@@ -62,6 +62,7 @@ class ArticlesController extends CommonController
     public function store(Request $request)
     {
         //
+        dd($request->all());
     }
 
     /**
--- resources/views/admin/articles/articles/add.blade.php
+++ resources/views/admin/articles/articles/add.blade.php
@@ -8,8 +8,10 @@
     <div class="layui-row" style="padding: 2px 15px 2px 15px">
         <br>
         <div style="color: #ec162d;size: 28px">@include('flash::message')</div>
-        <form class="layui-form layui-form-pane" method="post" action="{{url('admin/categorys')}}">
+        <form class="layui-form layui-form-pane" lay-filter="add" method="post" action="{{url('admin/articles')}}">
             {{csrf_field()}}
+            {{--作者id--}}
+            <input type="hidden" value="{{Auth::user()->id}}" name="user_id">
             <div class="layui-form-item">
                 <label class="layui-form-label">文章标题</label>
                 <div class="layui-input-inline">
@@ -19,50 +21,41 @@
 
             <div class="layui-form-item">
                 <label class="layui-form-label">缩略图</label>
-                <div class="layui-input-block">
+                <div class="layui-input-inline">
                     <input type="text" name="thumb" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input">
                 </div>
             </div>
 
-            <div class="layui-form-item">
-                <label class="layui-form-label">浏览次数</label>
-                <div class="layui-input-inline">
-                    <input type="text" name="cate_view" lay-verify="cate_view" placeholder="请输入密码" autocomplete="off" class="layui-input">
-                </div>
-                <div class="layui-form-mid layui-word-aux">必须填写数字但不能大于7位</div>
-            </div>
 
             <div class="layui-form-item">
+                <div class="layui-inline">
+                    <label class="layui-form-label">文章分类</label>
+                    <div class="layui-input-inline">
+                        <input type="text" name="category_id"  lay-verify="" autocomplete="off" class="layui-input">
+                    </div>
+                </div>
+                <div class="layui-inline">
+                    <label class="layui-form-label">浏览次数</label>
+                    <div class="layui-input-inline">
+                        <input type="text" name="cate_view"  lay-verify="" autocomplete="off" class="layui-input" disabled="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="layui-form-item" pane="">
                 <label class="layui-form-label">文章级别</label>
-                <div class="layui-input-inline">
-                    <input type="text" name="level" lay-verify="cate_view" placeholder="请输入密码" autocomplete="off" class="layui-input">
+                <div class="layui-input-block">
+                    <input type="radio" name="level" value=0 title="置顶" checked="">
+                    <input type="radio" name="level" value=1 title="推荐">
+                    <input type="radio" name="level" value=2 title="热门" >
                 </div>
-                <div class="layui-form-mid layui-word-aux">必须填写数字但不能大于7位</div>
             </div>
 
-
-            <div class="layui-form-item">
+            <div class="layui-form-item" pane="">
                 <label class="layui-form-label">文章状态</label>
-                <div class="layui-input-inline">
-                    <input type="text" name="state" lay-verify="cate_view" placeholder="请输入密码" autocomplete="off" class="layui-input">
+                <div class="layui-input-block">
+                    <input type="checkbox" value="1" name="state" lay-skin="switch" lay-filter="state" lay-text="通过|审核中">
                 </div>
-                <div class="layui-form-mid layui-word-aux">必须填写数字但不能大于7位</div>
-            </div>
-
-            <div class="layui-form-item">
-                <label class="layui-form-label">文章分类</label>
-                <div class="layui-input-inline">
-                    <input type="text" name="category_id" lay-verify="cate_view" placeholder="请输入密码" autocomplete="off" class="layui-input">
-                </div>
-                <div class="layui-form-mid layui-word-aux">必须填写数字但不能大于7位</div>
-            </div>
-
-            <div class="layui-form-item">
-                <label class="layui-form-label">文章作者</label>
-                <div class="layui-input-inline">
-                    <input type="text" name="user_id" lay-verify="cate_view" placeholder="请输入密码" autocomplete="off" class="layui-input">
-                </div>
-                <div class="layui-form-mid layui-word-aux">必须填写数字但不能大于7位</div>
             </div>
 
             <div class="layui-form-item">
@@ -91,7 +84,6 @@
                 <button class="layui-btn" lay-submit="" lay-filter="demo2">发布文章</button>
             </div>
         </form>
-
     </div>
 @endsection
 @section('js')
@@ -110,7 +102,15 @@
                 })*/
                 return true;
             });
-
+            //监听指定开关
+            form.on('switch(state)', function(data){
+                layer.tips('温馨提示：'+ (this.checked != 0 ? '直接通过审核！' : '不审核该文章！'), data.othis)
+            });
+            //初始只
+            form.val("add", {
+                "cate_view":0
+                ,"state": 0
+            })
 
         });
     </script>
